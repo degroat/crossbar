e98919cb2ebd7093b4be5951854e227a2deae43a
Chris DeGroat <chris.degroat@gmail.com>
2011-07-14 17:40:41 -0500

    some updates to the globals utility

--- globals.php
+++ globals.php
@@ -7,13 +7,34 @@ class globals
         $server_var = "_" . strtoupper($method);
         $var = $arguments[0];
 
-        if(empty($GLOBALS[$server_var][$var]))
+
+        $default = NULL;
+        if(isset($arguments[1]))
         {
-            if(!empty($arguments[1]))
+            $default = $arguments[1];
+        }
+
+
+        $overwrite_empty_string = FALSE;
+        if(isset($arguments[2]) && $arguments[2] == TRUE)
+        {
+            $overwrite_empty_string = TRUE;
+        }
+
+
+        if($overwrite_empty_string)
+        {
+            if(empty($GLOBALS[$server_var][$var]))
             {
-                return $arguments[1];
+                return $default;
             }
-            return NULL;
+        }
+        else
+        {
+            if(!isset($GLOBALS[$server_var][$var]))
+            {
+                return $default;
+            }
         }
         return $GLOBALS[$server_var][$var];
     }
